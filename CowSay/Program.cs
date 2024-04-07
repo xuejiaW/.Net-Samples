be79ead2d56fb55a4d05a44c79f123f2b9abbe18
XueJia Wang <wxjwangxuejia@outlook.com>
2024-04-07 14:56:51 +0800

    feat: add CowSay projects
    Signed-off-by: XueJia Wang <wxjwangxuejia@outlook.com>

---
+++ CowSay/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+
+namespace CowSay;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            string? versionString = Assembly.GetEntryAssembly()?
+               .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+               .InformationalVersion;
+
+            Console.WriteLine($"Cow Say v{versionString}");
+            Console.WriteLine("-------------");
+            Console.WriteLine("\nUsage:");
+            Console.WriteLine("  Cow Say <message>");
+            return;
+        }
+
+        ShowCow(string.Join(' ', args));
+    }
+
+    private static void ShowCow(string message)
+    {
+        string bot = $"\n        {message}";
+        bot += @"
+    __________________
+                         \
+                          \
+                             (oo)\_______
+                             (__)        )\/\
+                                 ||------||
+                                 ||      ||
+                            ";
+        Console.WriteLine(bot);
+    }
+}
